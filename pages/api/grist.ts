627cc29b5e6df93e7a37e226f01db946da21da14
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-02-28 12:23:21 +0100

    refactor: the Grist API route
    Also removes the limit for records

--- pages/api/grist.ts
+++ pages/api/grist.ts
@@ -1,24 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(
+export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse<unknown>
 ) {
   // NOTE: the need for manual JSON encoding of the filters is annoying: {"e-bez": ["Mitte"]}
   // NOTE: It's not exactly clear what is the tableId. I managed to find it through trial and error.
-  fetch(
-    `${process.env.NEXT_SECRET_GRIST_DOMAIN}/api/docs/33F9uWYkjdaz/tables/Kindertagesstatten/records?limit=100`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_SECRET_GRIST_API_KEY}`,
-      },
-    }
-  )
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_SECRET_GRIST_DOMAIN}/api/docs/33F9uWYkjdaz/tables/Kindertagesstatten/records`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_SECRET_GRIST_API_KEY}`,
+        },
       }
-      return response.json();
-    })
-    .then((response) => res.status(200).json(response));
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error: unknown) {
+    // @ts-ignore
+    return res.status(error.status || 500).end(error.message);
+  }
 }
