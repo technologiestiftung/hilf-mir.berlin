30aa6fe2569294bacb5964c6350aaf90713b85e3
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-05-03 17:29:30 +0200

    style: improve FacilityInfo + add email item

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 export const Sidebar: FC<{ isOpen: boolean }> = ({ isOpen, children }) => {
   return (
     <div
-      className={`absolute top-20 left-4 px-4 py-6 bg-white w-80 md:w-96 shadow-md z-20 overflow-y-auto transition-transform ${
+      className={`absolute top-20 left-4 p-4 bg-white w-80 md:w-96 shadow-md z-20 overflow-y-auto transition-transform ${
         isOpen ? "translate-x-0" : "-translate-x-96 !-left-4"
       }`}
       style={{ height: "calc(100% - 64px - 32px)" }}
