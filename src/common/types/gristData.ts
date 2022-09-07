244fbc44e17e363563b17a1f3ce8edfd9e18b273
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-09-07 18:41:18 +0200

    chore: fix types in test

--- src/common/types/gristData.ts
+++ src/common/types/gristData.ts
@@ -1,7 +1,6 @@
 /**
  * This type corresponds to the table row of psychological and other help facilities in Berlin. Note that the keys are subject to change whenever the columns are updated in the spreadsheet.
  */
-
 export interface TableRowType {
   id: number
   fields: {
@@ -48,31 +47,3 @@ export interface TableRowType {
     long2: number
   }
 }
-
-/**
- * This type corresponds to the table row of psychological and other help facilities in Berlin. Note that the keys are subject to change whenever the columns are updated in the spreadsheet.
- */
-/*
-export interface TableRowType {
-  id: number
-  fields: {
-    Projekt: string
-    Zuwendungsempfanger: string
-    Fordertyp: string
-    Angebotstyp: string
-    Zielgruppe: string
-    Leistung: string
-    Strasse: string
-    Hausnummer: string
-    Zusatz: string
-    PLZ: string
-    Bezirk: string
-    Telefonnummer?: string
-    EMail?: string
-    long2: number
-    Website: string
-    lat: number
-    district: string
-  }
-}
-*/
