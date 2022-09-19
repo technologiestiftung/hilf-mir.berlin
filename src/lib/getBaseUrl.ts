283bab7f88d6243a4daba72e28459b412f22e73a
Lucas Vogel <contact@vogelino.com>
2022-09-19 16:38:15 +0200

    Fix wrong use of env var in getBaseUrl function

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -1,6 +1,7 @@
 export const getBaseUrl = (): string => {
   const isProd = process.env.NODE_ENV === 'production'
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
+  const baseUrl =
+    process.env.NEXT_PUBLIC_FRONTEND_DOMAIN || 'http://localhost:3000'
   return isProd
     ? baseUrl
     : process.env.VERCEL_URL
