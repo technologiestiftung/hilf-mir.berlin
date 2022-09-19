99a2a222f0708efa9319385c575a126ce5e93ae3
Lucas Vogel <contact@vogelino.com>
2022-09-19 16:50:58 +0200

    Attempt to use NEXT_PUBLIC_VERCEL_URL

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -4,7 +4,7 @@ export const getBaseUrl = (): string => {
     process.env.NEXT_PUBLIC_FRONTEND_DOMAIN || 'http://localhost:3000'
   return isProd
     ? baseUrl
-    : process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
+    : process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : baseUrl
 }
