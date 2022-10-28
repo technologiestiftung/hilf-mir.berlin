dd2b6edd49e938da90ef9129141c619f3e262b7d
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-10-28 10:51:28 +0200

    feat(Icons): use new icon for "Schwangerschaft"

--- src/components/icons/tagIcons.ts
+++ src/components/icons/tagIcons.ts
@@ -1,21 +1,21 @@
 import { FC } from 'react'
-import BabyWagon from '../../images/filter-icons/baby-wagon.svg'
 import Blunt from '../../images/filter-icons/blunt.svg'
 import Dice from '../../images/filter-icons/dice.svg'
 import Drop from '../../images/filter-icons/drop.svg'
 import DrugPill from '../../images/filter-icons/drug-pill.svg'
 import DrugPills from '../../images/filter-icons/drug-pills.svg'
 import Ear from '../../images/filter-icons/ear.svg'
+import Fist from '../../images/filter-icons/fist.svg'
 import GameController from '../../images/filter-icons/game-controller.svg'
 import Heartbeat from '../../images/filter-icons/heartbeat.svg'
 import Hiv from '../../images/filter-icons/hiv.svg'
 import People from '../../images/filter-icons/people.svg'
 import Pizza from '../../images/filter-icons/pizza.svg'
+import PregnancyTest from '../../images/filter-icons/pregnancy-test.svg'
 import Stetoscope from '../../images/filter-icons/stetoscope.svg'
 import Transgender from '../../images/filter-icons/transgender.svg'
 import Wind from '../../images/filter-icons/wind.svg'
 import WineGlas from '../../images/filter-icons/wine-glas.svg'
-import Fist from '../../images/filter-icons/fist.svg'
 
 type IconElement = FC<{
   className?: string
@@ -23,21 +23,21 @@ type IconElement = FC<{
 }>
 
 export default {
-  'baby-wagon': BabyWagon as IconElement,
   blunt: Blunt as IconElement,
   dice: Dice as IconElement,
   drop: Drop as IconElement,
   'drug-pill': DrugPill as IconElement,
   'drug-pills': DrugPills as IconElement,
   ear: Ear as IconElement,
+  fist: Fist as IconElement,
   'game-controller': GameController as IconElement,
   heartbeat: Heartbeat as IconElement,
   hiv: Hiv as IconElement,
   people: People as IconElement,
   pizza: Pizza as IconElement,
+  'pregnancy-test': PregnancyTest as IconElement,
   stetoscope: Stetoscope as IconElement,
   transgender: Transgender as IconElement,
   wind: Wind as IconElement,
   'wine-glas': WineGlas as IconElement,
-  fist: Fist as IconElement,
 }
