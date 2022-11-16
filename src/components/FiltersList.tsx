9ffadd9da87a9d531673ac9d42cc9f85649b1517
Lucas Vogel <contact@vogelino.com>
2022-11-16 15:13:32 +0100

    refactor(WEG-130): Change no preference button for reset links

--- src/components/FiltersList.tsx
+++ src/components/FiltersList.tsx
@@ -37,6 +37,10 @@ export const FiltersList: FC<{
     tags.find((f) => f === targetGroup.id)
   )
 
+  const someGroupFiltersActive = labels
+    .filter(({ fields }) => fields.group2 !== 'zielpublikum')
+    .some(({ id }) => tags.find((f) => f === id))
+
   const filteredRecords = recordsWithOnlyLabels.filter((recordLabels) =>
     tags.every((tagId) => recordLabels.find((labelId) => labelId === tagId))
   )
@@ -59,48 +63,61 @@ export const FiltersList: FC<{
         <ul className="flex flex-wrap gap-2 place-content-start mb-5">
           {group2.map(renderLabel)}
         </ul>
-        <ul className="flex flex-wrap gap-2 place-content-start mb-8">
+        <ul className="flex flex-wrap gap-2 place-content-start mb-5">
           {group3.map(renderLabel)}
         </ul>
+        {someGroupFiltersActive && (
+          <button
+            onClick={() =>
+              updateFilters(
+                tags.filter((f) => {
+                  const label = labels.find(({ id }) => id === f)
+                  return label?.fields.group2 === `zielpublikum`
+                }) || []
+              )
+            }
+            className={classNames(
+              `text-lg leading-6 text-left font-normal mb-8`,
+              `focus:outline-none focus:ring-2 focus:ring-red`,
+              `focus:ring-offset-2 focus:ring-offset-white`,
+              `underline text-gray-80 hover-red transition-colors`
+            )}
+          >
+            {texts.reset}
+          </button>
+        )}
       </div>
       <div className="md:flex md:flex-wrap md:items-start md:gap-x-4">
-        <h3 className={classNames(`font-bold text-lg mb-3 w-full`)}>
+        <h3
+          className={classNames(
+            `font-bold text-lg mb-3 w-full flex justify-between`
+          )}
+        >
           {texts.filtersSearchTargetLabel}
         </h3>
-        <button
-          onClick={() =>
-            updateFilters(
-              tags.filter((f) => {
-                const label = labels.find(({ id }) => id === f)
-                return label?.fields.group2 !== `zielpublikum`
-              }) || []
-            )
-          }
-          className={classNames(
-            `py-1.5 border text-lg leading-6 pl-2 w-full md:w-auto mb-3`,
-            !someTargetFiltersActive &&
-              `bg-red border-red text-white font-bold pr-2.5`,
-            someTargetFiltersActive && ` border-gray-20 pr-3`,
-            `focus:outline-none focus:ring-2 focus:ring-red`,
-            `focus:ring-offset-2 focus:ring-offset-white`
-          )}
-        >
-          {texts.noTargetPreferenceButtonText}
-        </button>
-        <div
-          className={classNames(
-            `relative border-t border-gray-20 text-center uppercase text-lg`,
-            `font-medium tracking-widest text-gray-40 leading-4 mb-8 mt-5`,
-            `md:w-24`
-          )}
-        >
-          <span className="absolute -top-2 left-1/2 -translate-x-1/2 bg-white px-3">
-            {texts.orLabel}
-          </span>
-        </div>
-        <ul className="flex flex-wrap gap-2 mb-8">
+        <ul className="flex flex-wrap gap-2 mb-5">
           {targetGroups.map(renderLabel)}
         </ul>
+        {someTargetFiltersActive && (
+          <button
+            onClick={() =>
+              updateFilters(
+                tags.filter((f) => {
+                  const label = labels.find(({ id }) => id === f)
+                  return label?.fields.group2 !== `zielpublikum`
+                }) || []
+              )
+            }
+            className={classNames(
+              `text-lg leading-6 text-left font-normal mb-8`,
+              `focus:outline-none focus:ring-2 focus:ring-red`,
+              `focus:ring-offset-2 focus:ring-offset-white`,
+              `underline text-gray-80 hover-red transition-colors`
+            )}
+          >
+            {texts.reset}
+          </button>
+        )}
         <SwitchButton
           value={useGeolocation}
           onToggle={setGeolocationUsage}
