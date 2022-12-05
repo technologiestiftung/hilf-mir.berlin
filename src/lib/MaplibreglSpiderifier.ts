622c12ec699e08659850165eeded2dc0750b2b80
Lucas Vogel <contact@vogelino.com>
2022-12-05 14:38:02 +0100

    chore(MaplibreglSpiderifier): Fix formatting for prettier

--- src/lib/MaplibreglSpiderifier.ts
+++ src/lib/MaplibreglSpiderifier.ts
@@ -1,10 +1,4 @@
-import {
-  Marker,
-  Map,
-  LngLatLike,
-  PositionAnchor,
-  PointLike,
-} from 'maplibre-gl'
+import { Marker, Map, LngLatLike, PositionAnchor, PointLike } from 'maplibre-gl'
 
 interface SpiderParamType {
   x: number
