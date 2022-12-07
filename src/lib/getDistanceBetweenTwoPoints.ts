43d192a5fd24657c5a7c20eaf1f23c31be8a486b
Lucas Vogel <contact@vogelino.com>
2022-12-07 13:35:07 +0100

    refactor(getDistanceBetweenTwoPoints): Rename variables to generic names

--- src/lib/getDistanceBetweenTwoPoints.ts
+++ src/lib/getDistanceBetweenTwoPoints.ts
@@ -12,9 +12,9 @@ export function getDistanceBetweenTwoPoints(
   if (!pointA?.latitude || !pointA?.longitude) return
   if (!pointB?.latitude || !pointB?.longitude) return
 
-  const userLocation = new LngLat(pointA.longitude, pointA.latitude)
-  const facilityLocation = new LngLat(pointB.longitude, pointB.latitude)
-  const dist = userLocation.distanceTo(facilityLocation)
+  const pointALatLng = new LngLat(pointA.longitude, pointA.latitude)
+  const pointBLatLng = new LngLat(pointB.longitude, pointB.latitude)
+  const dist = pointALatLng.distanceTo(pointBLatLng)
 
   const distance = Math.round(dist / 100) / 10
   return distance
