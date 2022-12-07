385163945addcfd33533a7b22d79cdea63011818
Lucas Vogel <contact@vogelino.com>
2022-12-07 09:52:02 +0100

    refactor(getDistanceBetweenTwoPoints): Extract function into own util

--- src/lib/hooks/useDistanceToUser.ts
+++ src/lib/hooks/useDistanceToUser.ts
@@ -1,29 +1,12 @@
-import { LngLat } from 'maplibre-gl'
+import {
+  getDistanceBetweenTwoPoints,
+  PointType,
+} from '@lib/getDistanceBetweenTwoPoints'
 import { useCallback } from 'react'
 import { useUserGeolocation } from './useUserGeolocation'
 
-interface PointType {
-  latitude?: number
-  longitude?: number
-}
-
 interface UseDistanceToUserReturnType {
   getDistanceToUser: (pointA: PointType) => number | undefined
-}
-
-export function getDistanceBetweenTwoPoints(
-  pointA: PointType,
-  pointB: PointType
-): number | undefined {
-  if (!pointA?.latitude || !pointA?.longitude) return
-  if (!pointB?.latitude || !pointB?.longitude) return
-
-  const userLocation = new LngLat(pointA.longitude, pointA.latitude)
-  const facilityLocation = new LngLat(pointB.longitude, pointB.latitude)
-  const dist = userLocation.distanceTo(facilityLocation)
-
-  const distance = Math.round(dist / 100) / 10
-  return distance
 }
 
 export const useDistanceToUser = (): UseDistanceToUserReturnType => {
