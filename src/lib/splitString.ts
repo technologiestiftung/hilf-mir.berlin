2f06d9416fa3e2a450e72f921b1b504e3e544c24
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-01-30 14:32:22 +0100

    refactor(splitString): always run split function
    This way we always receive back an array of strings and don't have to handle different types

--- src/lib/splitString.ts
+++ src/lib/splitString.ts
@@ -1,12 +1,6 @@
-export const splitString = (
-  input: string,
-  separator: string
-): string | string[] => {
+export const splitString = (input: string, separator: string): string[] => {
   if (!separator) {
-    console.error(
-      'Please provide a separator character for splitting the input'
-    )
-    return input
+    console.warn('Please provide a separator character for splitting the input')
   }
 
   return input.split(separator)
