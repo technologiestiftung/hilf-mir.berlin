3f6fc58433a6ae1c07b4e3f7d21e033b05e1d7d8
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-02-27 17:40:52 +0100

    style(tags): fix hover color

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -26,7 +26,7 @@ export const Label: FC<{
           `py-1.5 text-lg flex gap-2 text-left leading-6 pl-2 pr-3 rounded`,
           isActive &&
             isInteractive &&
-            `bg-primary border-primary text-white hover:bg-primary hover:border-opacity-90 hover:bg-opacity-90`,
+            `bg-primary border-primary text-white hover:bg-purple-400`,
           (!isActive || !isInteractive) && ` border-gray-20`,
           !isActive &&
             isInteractive &&
