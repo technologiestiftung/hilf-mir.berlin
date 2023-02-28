666bf4f53534ed4510374d1622214557c0f2786d
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-02-28 16:02:03 +0100

    style: fix spacing

--- src/components/FiltersList.tsx
+++ src/components/FiltersList.tsx
@@ -73,7 +73,7 @@ export const FiltersList: FC<{
 
   return (
     <div className="pb-20 lg:pb-0">
-      <div className="md:pt-10 flex flex-wrap gap-x-8 md:pb-8">
+      <div className="md:pt-10 flex flex-wrap gap-x-8 pb-6 md:pb-8">
         <ul className="flex flex-wrap gap-2 place-content-start mb-5">
           <FiltersTagsList
             filters={[...group1, ...group2, ...group3]}
