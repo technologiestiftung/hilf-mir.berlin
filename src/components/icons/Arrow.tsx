7e55a09e98f51d223671710f556ad1cd62dfb60b
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-03-03 11:08:17 +0100

    chore: make icons resize properly

--- src/components/icons/Arrow.tsx
+++ src/components/icons/Arrow.tsx
@@ -13,6 +13,7 @@ export const Arrow: FC<{
     <svg
       width="25"
       height="26"
+      viewBox="0 0 25 26"
       xmlns="http://www.w3.org/2000/svg"
       className={classNames(className, ortientationClass)}
     >
--- src/components/icons/Globe.tsx
+++ src/components/icons/Globe.tsx
@@ -6,6 +6,7 @@ export const Globe: FC<{
   <svg
     width="24"
     height="24"
+    viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
     className={className}
   >
