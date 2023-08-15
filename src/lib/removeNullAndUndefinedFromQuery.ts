878db77739920df87bb5c9b375acd827efb8d87a
ff6347 <hash@ff6347.email>
2023-08-15 09:30:07 +0200

    style(eslint): Fix some linting errors

--- src/lib/removeNullAndUndefinedFromQuery.ts
+++ src/lib/removeNullAndUndefinedFromQuery.ts
@@ -1,7 +1,10 @@
 import { PageQueryType } from './mapRawQueryToState'
 
 export const removeNullAndUndefinedFromQuery = (
-  obj: Record<string, unknown | null>
+  obj: Record<
+    string,
+    null | undefined | string | number | boolean | Record<string, unknown>
+  >
 ): PageQueryType => {
   Object.keys(obj).forEach(
     (k) => (obj[k] === null || typeof obj[k] === 'undefined') && delete obj[k]
