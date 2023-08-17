f9627a2903c0dcc00cdc1cc9e50bd4c195275695
Lucas Vogel <contact@vogelino.com>
2023-08-17 20:35:20 +0200

    refactor(useMapHighlightMarker): Update missing useEffect dependendy

--- src/lib/hooks/useMapHighlightMarker.ts
+++ src/lib/hooks/useMapHighlightMarker.ts
@@ -45,6 +45,7 @@ export function useMapHighlightMarker(
     markerViewport?.longitude,
     markerViewport?.latitude,
     markerViewport?.zoom,
+    markerViewport?.color,
   ])
 
   return highlightedMarker.current
