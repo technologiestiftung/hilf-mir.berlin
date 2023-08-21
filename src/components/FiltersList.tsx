32ec42c7dcc953277f1fb1d9d638eaf054674f1b
Lucas Vogel <contact@vogelino.com>
2023-08-21 11:41:16 +0200

    fix(FiltersList): Fix prettier wrapping the showSubmitButton condition the wrong way

--- src/components/FiltersList.tsx
+++ src/components/FiltersList.tsx
@@ -81,6 +81,7 @@ export const FiltersList: FC<{
     updateUrlState({ tags: newTags })
   }
 
+  const showSubmitButton = pathname.trim().split('?')[0] === '/' || isMobile
   return (
     <div className="pb-20 lg:pb-0 @container">
       <div className="@md:pt-10 flex flex-wrap gap-x-8 pb-6 @md:pb-8">
@@ -198,47 +199,43 @@ export const FiltersList: FC<{
       >
         {texts.filtersGeoSearchLabel}
       </SwitchButton>
-      {pathname === '/' ||
-        (isMobile && (
-          <Button
-            scheme="primary"
-            size="large"
-            className={classNames(
-              'w-full @md:w-max @md:min-w-[324px]',
-              'group'
-            )}
-            href="/map"
-            query={latitude && longitude ? { latitude, longitude } : {}}
-            tag="a"
-            onClick={() => {
-              onSubmit()
-            }}
-            icon={
-              textSearchLoading ? (
-                <Spinner className={classNames('animate-spin')} />
-              ) : (
-                <Arrow
-                  className={classNames(
-                    'transition-transform group-hover:translate-x-0.5 group-disabled:group-hover:translate-x-0'
-                  )}
-                />
-              )
-            }
-            disabled={filteredFacilitiesCount === 0 || fieldsDisabled}
-            tooltip={
-              filteredFacilitiesCount === 0 && (
-                <span>{texts.filtersButtonTextFilteredNoResultsHint}</span>
-              )
-            }
-          >
-            {getSubmitText({
-              texts,
-              isLoading: textSearchLoading,
-              count: filteredFacilitiesCount,
-              total,
-            })}
-          </Button>
-        ))}
+      {showSubmitButton && (
+        <Button
+          scheme="primary"
+          size="large"
+          className={classNames('w-full @md:w-max @md:min-w-[324px]', 'group')}
+          href="/map"
+          query={latitude && longitude ? { latitude, longitude } : {}}
+          tag="a"
+          onClick={() => {
+            onSubmit()
+          }}
+          icon={
+            textSearchLoading ? (
+              <Spinner className={classNames('animate-spin')} />
+            ) : (
+              <Arrow
+                className={classNames(
+                  'transition-transform group-hover:translate-x-0.5 group-disabled:group-hover:translate-x-0'
+                )}
+              />
+            )
+          }
+          disabled={filteredFacilitiesCount === 0 || fieldsDisabled}
+          tooltip={
+            filteredFacilitiesCount === 0 && (
+              <span>{texts.filtersButtonTextFilteredNoResultsHint}</span>
+            )
+          }
+        >
+          {getSubmitText({
+            texts,
+            isLoading: textSearchLoading,
+            count: filteredFacilitiesCount,
+            total,
+          })}
+        </Button>
+      )}
     </div>
   )
 }
